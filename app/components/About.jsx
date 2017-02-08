b8c9575b75afeddd7b0de318f7bba1d567fb2261
Tim Arsenault <timarsen@gmail.com>
2017-02-08 15:37:22 -0700

    Style about page and add app description

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,8 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page</p>
+      <h1 className='text-center'>About</h1>
+      <blockquote>This is a simple weather app that uses the <strong>OpenWeather API</strong> to pull current weather conditions for any location searched by the user.</blockquote> <blockquote>I built this app to better learn the frontend framework <strong>React</strong> and the CSS framework <strong>Foundation</strong>.</blockquote>
     </div>
   )
 };
